1cd60e9e0c8bf4942066b0c26664548d6612b2a4
Konstantin <k.podyganov@mail.ru>
2020-04-29 19:06:42 +0300

    added form verification

--- src/components/forms/ClientsForm.jsx
+++ src/components/forms/ClientsForm.jsx
@@ -3,13 +3,61 @@ import React from 'react';
 import './FormStyle.css';
 
 export function ClientsForm() {
+  const [fio, setFio] = useState('');
+  const [register, setRegister] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const isFormValid = (fio, registration, birthday, phone) => {
+    if (fio.split(' ').length === 3 && typeof registration === 'string') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   return (
     <div>
-      <input type="text" className="form-field" placeholder="ФИО" />
-      <input type="text" className="form-field" placeholder="Данные о прописке" />
-      <input type="text" className="form-field" placeholder="Дата рождения" />
-      <input type="text" className="form-field" placeholder="Номер телефона" />
-      <button className="form-button">Добавить</button>
+      <input
+        type='text'
+        className='form-field'
+        placeholder='ФИО'
+        onInput={(e) => {
+          setFio(e.target.value);
+        }}
+      />
+      <input
+        type='text'
+        className='form-field'
+        placeholder='Данные о прописке'
+        onInput={(e) => {
+          setRegister(e.target.value);
+        }}
+      />
+      <input
+        type='text'
+        className='form-field'
+        placeholder='Дата рождения'
+        onInput={(e) => {
+          setBirthday(e.target.value);
+        }}
+      />
+      <input
+        type='text'
+        className='form-field'
+        placeholder='Номер телефона'
+        onInput={(e) => {
+          setPhone(e.target.value);
+        }}
+      />
+      <button
+        className='form-button'
+        onClick={() => {
+          isFormValid(fio, register, birthday, phone);
+        }}
+      >
+        Добавить
+      </button>
     </div>
   );
 }
--- src/components/forms/ServicesForm.jsx
+++ src/components/forms/ServicesForm.jsx
@@ -3,12 +3,52 @@ import React from 'react';
 import './FormStyle.css';
 
 export function ServicesForm() {
+  const [service, setService] = useState('');
+  const [cost, setCost] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  const isFormValid = (serviceName, cost, duration) => {
+    if (typeof serviceName === 'string') {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   return (
     <div>
-      <input type="text" className="form-field" placeholder="Название услуги" />
-      <input type="text" className="form-field" placeholder="Цена" />
-      <input type="text" className="form-field" placeholder="Продолжительность" />
-      <button className="form-button">Добавить</button>
+      <input
+        type='text'
+        className='form-field'
+        placeholder='Название услуги'
+        onInput={(e) => {
+          setService(e.target.value);
+        }}
+      />
+      <input
+        type='text'
+        className='form-field'
+        placeholder='Цена'
+        onInput={(e) => {
+          setCost(e.target.value);
+        }}
+      />
+      <input
+        type='text'
+        className='form-field'
+        placeholder='Продолжительность'
+        onInput={(e) => {
+          setDuration(e.target.value);
+        }}
+      />
+      <button
+        className='form-button'
+        onClick={() => {
+          isFormValid(service, cost, duration);
+        }}
+      >
+        Добавить
+      </button>
     </div>
   );
 }
