c57908b9de256701f43064d722b0a875a8c177db
Konstantin <k.podyganov@mail.ru>
2020-05-21 20:30:13 +0300

    added records page to project

---
+++ src/pages/RecordsPage.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { Sidebar } from '../components/sidebar/Sidebar';
+import { Header } from '../components/Header';
+import { Panel } from '../components/Panel';
+import { Wrap } from '../components/containers/Wrap';
+import { Container } from '../components/containers/Container';
+import { Content } from '../components/containers/Content';
+import { Page } from '../components/containers/Page';
+import { RightSidebar } from '../components/containers/RightSidebar';
+
+import { Table, TableRow, TableHeader } from '../components/Table';
+
+const recordsList = [
+  {
+    id: 0,
+    clientFio: 'Артёмов Артём Артёмович',
+    doctorFio: 'Иванов Иван Иванович',
+    serviceName: 'Удаление зуба',
+    date: '21.05.2020',
+  },
+];
+
+export function RecordsPage() {
+  return (
+    <Page>
+      <Sidebar />
+
+      <Wrap>
+        <Header title="Заявки" />
+
+        <Container>
+          <Content>
+            <TableHeader values={['ФИО клиента', 'ФИО врача', 'Услуга', 'Дата и время записи']} />
+            {recordsList.map((record) => (
+              <TableRow key={record.id} values={[record.clientFio, record.doctorFio, record.serviceName, record.date]} />
+            ))}
+          </Content>
+
+          <RightSidebar></RightSidebar>
+        </Container>
+      </Wrap>
+    </Page>
+  );
+}
